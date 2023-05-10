bb8878ab3e37982d0568ddf2c4de7b8c3aa28b2a
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-05-10 17:56:15 +0400

    change eslint to cjs

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -8,6 +8,6 @@ dotenv.config({ path: resolve(cwd(), '.env') })
 // eslint-disable-next-line node/no-process-env
 export default cleanEnv(process.env, {
   DOMAIN: str({ default: undefined }),
-  IPFS: str({ default: 'http://ipfs:5001/api/v0' }),
+  IPFS: str({ default: 'http://127.0.0.1:5001/api/v0' }),
   PORT: num({ default: 1337 }),
 })
