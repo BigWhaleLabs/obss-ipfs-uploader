b116c5fb57631fc62903d459beaafbe7ad56f2a7
Wilson Lau <wilson@bigwhalelabs.com>
2023-09-28 09:53:03 -0400

    fix: extract defaultResizeOptions

--- src/helpers/resizeImage.ts
+++ src/helpers/resizeImage.ts
@@ -1,14 +1,16 @@
 import sharp from 'sharp'
 
+const defaultResizeOptions: sharp.ResizeOptions = {
+  fit: 'cover',
+  withoutEnlargement: true,
+}
+
 export default async function resizeImage(
   originalImage: Parameters<typeof sharp>[0],
   height?: number,
   width?: number
 ): Promise<Buffer> {
-  const resizeOptions: sharp.ResizeOptions = {
-    fit: 'cover',
-    withoutEnlargement: true,
-  }
+  const resizeOptions = { ...defaultResizeOptions }
   if (height) resizeOptions.height = height
   if (width) resizeOptions.width = width
   const resized = await sharp(originalImage).resize(resizeOptions).toBuffer()
