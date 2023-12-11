eec1d4ba8f3f66ab85ea4dba26ce6a92d4c9a61a
Wilson Lau <wilson@bigwhalelabs.com>
2023-12-11 16:26:22 -0500

    feat: update script to check existing pins

--- src/scripts/backup-cid.ts
+++ src/scripts/backup-cid.ts
@@ -1,11 +1,21 @@
+import axios from 'axios'
 import backupCID from '../helpers/backupCID'
+import env from '../helpers/env'
 import fs from 'fs'
 
 async function main() {
+  const res = await axios.get(
+    'https://api.filebase.io/v1/ipfs/pins?limit=30000',
+    { headers: { Authorization: `Bearer ${env.FILEBASE_API_TOKEN}` } }
+  )
+  const backedUpPins = res.data.results.map(({ pin }: any) => pin.cid)
+  const backupUpPinSet = new Set(backedUpPins)
   const cids = await fs
     .readFileSync('./src/scripts/pins-new-node.txt', 'utf-8')
     .split('\n')
-  for (const cid of cids) await backupCID(cid.trim())
+  for (const cid of cids) {
+    if (!backupUpPinSet.has(cid)) await backupCID(cid.trim())
+  }
 }
 
 main()
