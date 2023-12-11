fa1483a5c0d5606c7c8d38742350897b829304b8
Wilson Lau <wilson@bigwhalelabs.com>
2023-12-11 16:30:00 -0500

    chore: fix types

--- src/scripts/backup-cid.ts
+++ src/scripts/backup-cid.ts
@@ -3,12 +3,24 @@ import backupCID from '../helpers/backupCID'
 import env from '../helpers/env'
 import fs from 'fs'
 
+interface GetPinsResult {
+  pin: {
+    name: string
+    cid: string
+  }
+}
+
+interface GetPinsResponse {
+  count: number
+  results: GetPinsResult[]
+}
+
 async function main() {
-  const res = await axios.get(
+  const res = await axios.get<GetPinsResponse>(
     'https://api.filebase.io/v1/ipfs/pins?limit=30000',
     { headers: { Authorization: `Bearer ${env.FILEBASE_API_TOKEN}` } }
   )
-  const backedUpPins = res.data.results.map(({ pin }: any) => pin.cid)
+  const backedUpPins = res.data.results.map(({ pin }) => pin.cid)
   const backupUpPinSet = new Set(backedUpPins)
   const cids = await fs
     .readFileSync('./src/scripts/pins-new-node.txt', 'utf-8')
